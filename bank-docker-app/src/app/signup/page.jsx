7661f4c7eff6e7684a330f5a8706bbebbfb66bd2
axelcfk <131769961+axelcfk@users.noreply.github.com>
2024-04-16 09:00:58 +0200

    url-test

--- bank-docker-app/src/app/signup/page.jsx
+++ bank-docker-app/src/app/signup/page.jsx
@@ -15,7 +15,7 @@ export default function SignUp() {
     e.preventDefault();
 
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`, {
+      const response = await fetch(`http://13.60.52.124:5009/users`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
